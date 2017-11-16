ab30f321098a516f94a85d19794b92eb4f8d0cd7
joseveronez <jozeveronez@gmail.com>
2017-11-16 16:12:33 -0200

    Site
    Página Home

--- views/home.php
+++ views/home.php
@@ -2,10 +2,10 @@
     .banner-home {background-image: url(<?= RAIZSITE ?>/imagens/advogado.jpg); background-position: center center; background-repeat: no-repeat;background-size: 100% 100%; height: 93vh;}
 </style>
 <div class="owl-carousel margem-header-home" id="owl">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12  padding-zero banner-home branco-fonte">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 banner-home branco-fonte">
         <div class="container">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT30p">
-                <h1 class="size60 Light">Ferraz Cicarelli</h1>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT30p">
+                <h3 class="size60 Light">Ferraz Cicarelli</h3>
                 <p class="size18">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>Suspendisse aliquam vel velit sollicitudin porta.Suspendisse tincidunt nibh.</p>
                 <a role="button" href="" class="btn btn-home MarginT7p">LOREM IPSUM</a>
             </div>
@@ -14,23 +14,82 @@
 </div>
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero branco-fonte">
     <div class="container">
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
-            <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive" >
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT10p MarginB10p padding-zero">
+            <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 ">
+                <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive" style="margin: 0 auto;">
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 ">
+                <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive" style="margin: 0 auto;">
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 ">
+                <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive" style="margin: 0 auto;">
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 ">
+                <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive" style="margin: 0 auto;">
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 ">
+                <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive" style="margin: 0 auto;">
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 ">
+                <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive" style="margin: 0 auto;">
+            </div>
         </div>
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
-            <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive" >
+    </div>
+</div>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero branco-fonte">
+    <div class="container">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 preto-fonte MarginB5p">
+            <h3 class="text-center size30 Light">Lorem ipsum dolor sit amet</h3>
         </div>
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
-            <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive" >
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 preto-fonte">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB3p">
+                <h4>Lorem ipsum dolor sit amet</h4>
+                <h5 class="texto-empresa">Consectetur adipiscing elit. Suspendisse aliquam vel velit sollicitudin porta. Suspendisse tincidunt nibh ac lectus dapibus vestibulum. Sed ultricies ac arcu ut efficitur.</h5>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB3p">
+                <h4>Lorem ipsum dolor sit amet</h4>
+                <h5 class="texto-empresa">Consectetur adipiscing elit. Suspendisse aliquam vel velit sollicitudin porta. Suspendisse tincidunt nibh ac lectus dapibus vestibulum. Sed ultricies ac arcu ut efficitur.</h5>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB3p">
+                <h4>Lorem ipsum dolor sit amet</h4>
+                <h5 class="texto-empresa">Consectetur adipiscing elit. Suspendisse aliquam vel velit sollicitudin porta. Suspendisse tincidunt nibh ac lectus dapibus vestibulum. Sed ultricies ac arcu ut efficitur.</h5>
+            </div>
         </div>
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
-            <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive" >
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 preto-fonte ">
+            <iframe class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" height="315" src="https://www.youtube.com/embed/zqE-ultsWt0" frameborder="0" allowfullscreen></iframe>
         </div>
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
-            <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive" >
+    </div>
+</div>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT10p preto-fonte">
+    <div class="container">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            <img src="<?= RAIZSITE ?>/imagens/socio.png" style="margin: 0 auto; width: 100%;">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero PaddingT10p PaddingB10p border-socios">
+                <h4 class="text-center">Lorem Ipsum</h4>
+                <h6 class="text-center Light">Lorem ipsum dolor sit amet</h6>
+                <hr class="hr-socios">
+                <a target="_blank" href="#">
+                <div class="btn-group">            
+                    <i class="fa fa-twitter branco-fonte"></i>
+                        </a>
+                </div>
+            </div>
         </div>
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
-            <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive" >
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            <img src="<?= RAIZSITE ?>/imagens/socio.png" style="margin: 0 auto; width: 100%;">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero PaddingT10p PaddingB10p border-socios">
+                <h4 class="text-center">Lorem Ipsum</h4>
+                <h6 class="text-center Light">Lorem ipsum dolor sit amet</h6>
+                <hr class="hr-socios">
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            <img src="<?= RAIZSITE ?>/imagens/socio.png" style="margin: 0 auto; width: 100%;">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero PaddingT10p PaddingB10p border-socios">
+                <h4 class="text-center">Lorem Ipsum</h4>
+                <h6 class="text-center Light">Lorem ipsum dolor sit amet</h6>
+                <hr class="hr-socios">
+            </div>
         </div>
     </div>
 </div>
