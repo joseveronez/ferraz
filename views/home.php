a405eb3e05e3caa7cd96c3b2c46f475093414144
joseveronez <jozeveronez@gmail.com>
2017-11-17 18:31:28 -0200

    Site
    Area depoimentos

--- views/home.php
+++ views/home.php
@@ -1,8 +1,9 @@
 <style>
-    .banner-home {background-image: url(<?= RAIZSITE ?>/imagens/advogado.jpg); background-position: center center; background-repeat: no-repeat;background-size: 100% 100%; height: 93vh;}
+    .banner-home {background-image: url(<?= RAIZSITE ?>/imagens/advogado.jpg); background-position: center center; background-repeat: no-repeat;background-size: 100% 100%; height: 93vh; background-attachment: fixed;}
 </style>
-<div class="owl-carousel margem-header-home" id="owl">
+
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 banner-home branco-fonte">
+        <div class="owl-carousel margem-header-home" id="owl">
         <div class="container">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT30p">
                 <h3 class="size60 Light">Ferraz Cicarelli</h3>
@@ -68,10 +69,17 @@
                 <h4 class="text-center">Lorem Ipsum</h4>
                 <h6 class="text-center Light">Lorem ipsum dolor sit amet</h6>
                 <hr class="hr-socios">
-                <a target="_blank" href="#">
-                <div class="btn-group">            
-                    <i class="fa fa-twitter branco-fonte"></i>
-                        </a>
+                
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 col-xs-offset-3 text-center">
+                    <a href="" role="button" class="btn bg-preto">
+                        <i class="fa fa-twitter branco-fonte"></i>
+                    </a>
+                    <a href="" role="button" class="btn bg-preto">
+                        <i class="fa fa-linkedin branco-fonte"></i>
+                    </a>
+                    <a href="" role="button" class="btn bg-preto">
+                        <i class="fa fa-instagram branco-fonte"></i>
+                    </a>
                 </div>
             </div>
         </div>
@@ -81,6 +89,17 @@
                 <h4 class="text-center">Lorem Ipsum</h4>
                 <h6 class="text-center Light">Lorem ipsum dolor sit amet</h6>
                 <hr class="hr-socios">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 col-xs-offset-3 text-center">
+                    <a href="" role="button" class="btn bg-preto">
+                        <i class="fa fa-twitter branco-fonte"></i>
+                    </a>
+                    <a href="" role="button" class="btn bg-preto">
+                        <i class="fa fa-linkedin branco-fonte"></i>
+                    </a>
+                    <a href="" role="button" class="btn bg-preto">
+                        <i class="fa fa-instagram branco-fonte"></i>
+                    </a>
+                </div>
             </div>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
@@ -89,8 +108,43 @@
                 <h4 class="text-center">Lorem Ipsum</h4>
                 <h6 class="text-center Light">Lorem ipsum dolor sit amet</h6>
                 <hr class="hr-socios">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 col-xs-offset-3 text-center">
+                    <a href="" role="button" class="btn bg-preto">
+                        <i class="fa fa-twitter branco-fonte"></i>
+                    </a>
+                    <a href="" role="button" class="btn bg-preto">
+                        <i class="fa fa-linkedin branco-fonte"></i>
+                    </a>
+                    <a href="" role="button" class="btn bg-preto">
+                        <i class="fa fa-instagram branco-fonte"></i>
+                    </a>
+                </div>
             </div>
         </div>
+    </div>
+</div>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero bg-cinza-claro">
+    <h3 class="size30 text-center Light MarginT20p MarginB10p">Depoimentos</h3>
+    <div class="owl-carousel" id="owl">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+            <div class="container">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <img src="<?= RAIZSITE ?>/imagens/foto.png" class="img-responsive" style="margin: 0 auto; height: 100px; width: 100px;border-radius: 50%; ">
+                    <h5 class="size18 Light MarginT5p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquam vel velit sollicitudin porta.Suspendisse tincidunt nibh.</h5>
+                    <h6 class="size12 Light MarginT5p MarginB20p">Lorem ipsum</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT10p preto-fonte">
+    <div class="container">
+        
+    </div>
+</div>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero bloco-mapa">
+    <div class="google-maps">
+        <iframe id="iframeMaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3603.4265251941215!2d-49.29276078446682!3d-25.42400003888302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce3fbc745e1e3%3A0xa29ca15da95633b4!2sTravessa+Amando+Mann+-+Merc%C3%AAs%2C+Curitiba+-+PR!5e0!3m2!1spt-BR!2sbr!4v1510950063027" style="height: 450px; width: 100%;background-attachment: fixed; border: none; "></iframe>
     </div>
 </div>
 <script type="text/javascript">
