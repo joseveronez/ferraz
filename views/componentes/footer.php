1e137261a322d786e0d8caec3f5116812679f604
joseveronez <jozeveronez@gmail.com>
2017-11-20 16:47:21 -0200

    Footer
    Criação do footer

--- views/componentes/footer.php
+++ views/componentes/footer.php
@@ -1,4 +1,25 @@
 <footer>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
+        <div class="container">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 padding-zero">
+                <img src="<?= RAIZSITE ?>/imagens/teste.png" class="img-responsive">
+                <p class="Light">Cras vitae accumsan dui. Quisque aliquet in nulla quis ultrices. Etiam et mollis nisi. Nullam quis felis lacinia, molestie sem vitae, congue est. Morbi egestas erat suscipit hendrerit molestie. Morbi metus urna, sodales non quam in, porta luctus turpis. Nunc cursus eu justo ac dictum. </p>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                <h5>Lorem Ipsum</h5>
+                <ul class="list-unstyled">
+                    <li class="MarginT2p MarginB2p">Lorem Ipsum</li>
+                    <li class="MarginT2p MarginB2p">Lorem Ipsum</li>
+                    <li class="MarginT2p MarginB2p">Lorem Ipsum</li>
+                    <li class="MarginT2p MarginB2p">Lorem Ipsum</li>
+                    <li class="MarginT2p MarginB2p">Lorem Ipsum</li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 padding-zero">
+                
+            </div>
+        </div>
+    </div>
 </footer>
 </body>
 </html>
